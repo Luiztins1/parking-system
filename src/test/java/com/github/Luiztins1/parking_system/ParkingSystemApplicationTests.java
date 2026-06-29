bac850e3f23146d40b87016d3b5828f97c73b6de
Luiz Gabriel de Almeida Martins Marmorecola <luizgabrielmartins2005@gmail.com>
2026-06-29 20:08:04 -0300

    init.

---
+++ src/test/java/com/github/Luiztins1/parking_system/ParkingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Luiztins1.parking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
