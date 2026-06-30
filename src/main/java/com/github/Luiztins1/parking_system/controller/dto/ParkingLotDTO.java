86515f79c6a9347a0ddd9bd26513d8618ad53d37
Luiz Gabriel de Almeida Martins Marmorecola <luizgabrielmartins2005@gmail.com>
2026-06-30 18:07:57 -0300

    feat: add layer service. There was a complete change in the architecture. They analyzed how fast the system should work. With that, the main logic was centralized in the Ticket entity.

--- src/main/java/com/github/Luiztins1/parking_system/controller/dto/ParkingLotDTO.java
+++
@@ -1,9 +0,0 @@
-package com.github.Luiztins1.parking_system.controller.dto;
-
-import java.util.UUID;
-
-public record ParkingLotDTO(
-        UUID id,
-        Integer totalSpots,
-        Integer occupiedSpots) {
-}
